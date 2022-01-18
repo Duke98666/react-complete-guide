7b39e2d1e25a6bc9716b739e55bf9039fdfc3478
Dinakaran S <dinakarans98666@gmail.com>
2022-01-18 23:07:21 +0530

    Finished the project

---
+++ src/store/cart-context.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CartContext = React.createContext({
+  items: [],
+  totalAmount: 0,
+  addItem: (item) => {},
+  rmeoveItem: (id) => {},
+});
+
+export default CartContext;
